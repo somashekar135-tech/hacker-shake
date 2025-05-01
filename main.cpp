65ad21c952941954b8305af156b910f8fc3275a9
somashekar <somashekar13052005@gmail.com>
2025-05-01 22:12:07 +0530

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
-#include<iostream>
+#include <iostream>
+#define SIZE 5
 using namespace std;
-int main(){
 
+class LinearQueue {
+    int arr[SIZE];
+    int front, rear;
 
-cout<<"hello world"
-}
+public:
+    LinearQueue() {
+        front = rear = -1;
+    }
+
+    bool isFull() {
+        return rear == SIZE - 1;
+    }
+
+    bool isEmpty() {
+        return front == -1 || front > rear;
+    }
+
+    void enqueue(int x) {
+        if (isFull()) {
+            cout << "Queue Overflow\n";
+            return;
+        }
+        if (isEmpty()) front = 0;
+        arr[++rear] = x;
+        cout << x << " inserted\n";
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+            cout << "Queue Underflow\n";
+            return;
+        }
+        cout << arr[front++] << " removed\n";
+    }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Queue is empty\n";
+            return;
+        }
+        for (int i = front; i <= rear; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
